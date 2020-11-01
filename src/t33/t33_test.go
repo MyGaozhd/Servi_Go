4388a4c058e89ed8fbd50f984914bf11f90c0a0b
高志东 <gaozhdf@yonyou.com>
2020-11-01 16:29:11 +0800

    并发编程-仅需要任意任务完成

--- src/t33/t33_test.go
+++ src/t33/t33_test.go
@@ -2,12 +2,15 @@ package t33
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 	"time"
 )
 
 func Test33_0(t *testing.T) {
+	t.Log("start->", runtime.NumGoroutine())
 	t.Log(firstResponse())
+	t.Log("end->", runtime.NumGoroutine())
 }
 
 func runTask(id int) string {
@@ -19,7 +22,21 @@ func runTask(id int) string {
   只需要任意任务完成就返回
 */
 func firstResponse() string {
-	ch := make(chan string)
+	/**
+	写法一：
+	  ch := make(chan string)此写法会出现协程泄露
+	运行结果一：
+	  t33_test.go:11: start-> 2
+	  t33_test.go:12: the result is from 2
+	  t33_test.go:13: end-> 11
+	写法二：
+	  ch := make(chan string, 10)防止协程泄露
+	运行结果二：
+	  t33_test.go:11: start-> 2
+	  t33_test.go:12: the result is from 3
+	  t33_test.go:13: end-> 2 **重点在此处**
+	*/
+	ch := make(chan string, 10)
 	for i := 0; i < 10; i++ {
 		go func(i int) {
 			ret := runTask(i)
